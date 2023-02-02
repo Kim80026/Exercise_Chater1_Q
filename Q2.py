d99bc745907e8c526f784e4f2e547868c0e8e35d
k80026 <k80026@gmail.com>
2023-02-02 16:05:38 +0900

    Q2

---
+++ Q2.py
@@ -0,0 +1,18 @@
+# Q2 Answer template
+# Q2 (https://www.acmicpc.net/problem/1110)
+
+N = int(input())
+tmp = N  #첫번째 값과 비교하기위해 tmp에 N을 저장
+tens = N // 10 #10의 자리값
+ones = N % 10 #1의 자리값
+count = 0
+
+while 1:
+    N = tens + ones #새로운 수 만들기
+    count += 1
+    tens = ones #주어진 수 가장오른쪽 자리 수
+    ones = N % 10 #새로운 수 가장오른쪽 자리 수
+    if tens * 10 + ones == tmp:
+        break
+    
+print(count)
